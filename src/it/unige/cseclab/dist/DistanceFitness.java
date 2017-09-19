45e5c9d9010f0a074eb80aa6f68a83ac260bf6a3
gabriele <yugo.amaril@gmail.com>
2017-09-19 18:10:37 +0200

    GA predicate distance added

---
+++ src/it/unige/cseclab/dist/DistanceFitness.java
@@ -0,0 +1,51 @@
+package it.unige.cseclab.dist;
+
+import java.util.Map;
+
+import com.lagodiuk.ga.Fitness;
+
+import it.unige.cseclab.pred.Predicate;
+
+public class DistanceFitness implements Fitness<EnvChromosome, Double> {
+	
+	Map<String,Object> ref;
+	Predicate p;
+
+	public DistanceFitness(Predicate p, Map<String, Object> ref) {
+		super();
+		this.ref = ref;
+		this.p = p;
+	}
+
+	@Override
+	public Double calculate(EnvChromosome c) {
+		
+		if(!p.isSolution(c.env)) {
+			return Double.MAX_VALUE;
+		}
+		
+		double sum = 0;
+		
+		for(String k : ref.keySet()) {
+			sum += Math.pow(distance(ref.get(k), c.env.get(k)), 2);
+		}
+		
+		return Math.sqrt(sum);
+	}
+
+	private double distance(Object a, Object b) {
+		if(a instanceof Boolean) {
+			return DefaultDistance.b().d((Boolean)a, (Boolean)b);
+		} else if(a instanceof Integer) {
+			return DefaultDistance.i().d((Integer)a, (Integer)b);
+		} else if(a instanceof Double) {
+			return DefaultDistance.d().d((Double)a, (Double)b);
+		} else if(a instanceof String) {
+			return DefaultDistance.s().d((String)a, (String)b);
+		} else {
+			return DefaultDistance.o().d(a, b);
+		}
+	}
+	
+
+}
---
+++ test/it/unige/cseclab/dist/TestPredicateDistance.java
@@ -0,0 +1,81 @@
+package it.unige.cseclab.dist;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+import org.junit.Test;
+import org.smtlib.IExpr;
+import org.smtlib.SMT;
+import org.smtlib.IExpr.ISymbol;
+
+import it.unige.cseclab.pred.Predicate;
+
+public class TestPredicateDistance {
+	
+	@Test
+	public void checkPred() {
+		
+IExpr.IFactory efact = (new SMT()).smtConfig.exprFactory;
+		
+		String sp = "p";
+		String sq = "q";
+		String sr = "r";
+
+		ISymbol p = efact.symbol(sp);
+		ISymbol q = efact.symbol(sq);
+		ISymbol r = efact.symbol(sr);		
+		
+		IExpr pc = efact.fcn(
+				efact.symbol("and"), 
+				efact.fcn(
+						efact.symbol(">"), 
+						p,
+						efact.symbol("2")),
+				efact.fcn(
+						efact.symbol("<"), 
+						p,
+						efact.symbol("5"))
+				);
+		IExpr qc = efact.fcn(
+				efact.symbol("or"), 
+				efact.fcn(
+						efact.symbol(">"), 
+						q,
+						efact.symbol("3")),
+				efact.fcn(
+						efact.symbol("<"), 
+						q,
+						efact.symbol("1"))
+				);
+		IExpr rc = efact.fcn(
+						efact.symbol(">"), 
+						r,
+						efact.symbol("6"));
+		
+		Vector<String> symbs = new Vector<>();
+		Vector<IExpr> exprs = new Vector<>();	
+		
+		symbs.add(sp);
+		symbs.add(sq);
+		symbs.add(sr);
+		
+		exprs.add(pc);
+		exprs.add(qc);
+		exprs.add(rc);
+		
+		Predicate pred = new Predicate(exprs, symbs);
+		
+		PredicateDistance pd = new PredicateDistance(pred);
+		
+		Map<String,Object> x = new HashMap<>();
+		x.put(sp, new Integer(7));
+		x.put(sq, new Integer(7));
+		x.put(sr, new Integer(1));
+		
+		double res = pd.d(x);
+		
+		System.out.println("Distance = " + res);
+	}
+
+}
